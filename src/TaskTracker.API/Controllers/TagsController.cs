cc81268a03488cc71e847728bba7c42271319489
winrarpaidversion <leadion48@gmail.com>
2026-02-23 06:22:36 +0800

    add tagscontroller and service

---
+++ src/TaskTracker.API/Controllers/TagsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskTracker.Application.DTOs;
+using TaskTracker.Application.Services;
+
+namespace TaskTracker.API.Controllers
+{
+    [Route("api/tags")]
+    [ApiController]
+    [Produces("application/json")]
+    public class TagsController : ControllerBase
+    {
+        private readonly ITagService _tagService;
+
+        public TagsController(ITagService tagService) 
+        {
+            _tagService = tagService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(TagDto), 200)]
+        public async Task<ActionResult<List<TagDto>>> GetTags()
+        {
+            var tags = await _tagService.GetTagsAsync();
+
+            return Ok(tags);
+        }
+    }
+}
